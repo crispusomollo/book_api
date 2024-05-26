24545ac0faf29ea9254668ef075a9b2313134841
crispusomollo <crispusomollo@gmail.com>
2024-05-26 06:15:23 +0000

    Created the main application file

---
+++ api/v1/app.py
@@ -0,0 +1,14 @@
+# book_api/app.py
+
+from flask import Flask
+from api.v1.views.books import books_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(books_bp, url_prefix='/api/v1')
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
+
