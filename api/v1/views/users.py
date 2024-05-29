8efe1d24a770e8944dca674c992572742d8b91ee
crispusomollo <crispusomollo@gmail.com>
2024-05-29 13:59:25 +0000

    redefined the users route

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,40 +1,29 @@
-# book_api/api/v1/views/books.py
-
 from flask import Blueprint, request, jsonify
-from models.book import Book
+from models.user import User
 
-books_bp = Blueprint('books', __name__)
+user_bp = Blueprint('user', __name__)
 
-@books_bp.route('/books', methods=['GET'])
-def get_books():
-    books = Book.get_all_books()
-    return jsonify(books), 200
+@user_bp.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    User.create_user(data)
+    return jsonify({"message": "User created successfully"}), 201
 
-@books_bp.route('/books/<int:book_id>', methods=['GET'])
-def get_book(book_id):
-    book = Book.get_book_by_id(book_id)
-    if book:
-        return jsonify(book), 200
-    return jsonify({"error": "Book not found"}), 404
+@user_bp.route('/users/<username>', methods=['GET'])
+def get_user(username):
+    user = User.get_user(username)
+    if user:
+        return jsonify(user), 200
+    return jsonify({"message": "User not found"}), 404
 
-@books_bp.route('/books', methods=['POST'])
-def add_book():
-    book_data = request.get_json()
-    Book.add_book(book_data)
-    return jsonify({"message": "Book added successfully"}), 201
+@user_bp.route('/users/<username>', methods=['PUT'])
+def update_user(username):
+    data = request.get_json()
+    User.update_user(username, data)
+    return jsonify({"message": "User updated successfully"}), 200
 
-@books_bp.route('/books/<int:book_id>', methods=['PUT'])
-def update_book(book_id):
-    update_data = request.get_json()
-    result = Book.update_book(book_id, update_data)
-    if result.matched_count:
-        return jsonify({"message": "Book updated successfully"}), 200
-    return jsonify({"error": "Book not found"}), 404
-
-@books_bp.route('/books/<int:book_id>', methods=['DELETE'])
-def delete_book(book_id):
-    result = Book.delete_book(book_id)
-    if result.deleted_count:
-        return jsonify({"message": "Book deleted successfully"}), 200
-    return jsonify({"error": "Book not found"}), 404
+@user_bp.route('/users/<username>', methods=['DELETE'])
+def delete_user(username):
+    User.delete_user(username)
+    return jsonify({"message": "User deleted successfully"}), 200
 
