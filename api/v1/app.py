e492e7b5c181ee01d1873ff9bfbb7bbe9708e95d
crispusomollo <crispusomollo@gmail.com>
2024-05-29 14:10:47 +0000

    updated the route for the books

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,23 @@
 # book_api/app.py
 
 from flask import Flask
-from api.v1.views.books import books_bp
+from config.settings import Config
+from models.user import mongo
+from api.v1.views.users import user_bp
+from api.v1.views.books import book_bp
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(books_bp, url_prefix='/api/v1')
+    app.config.from_object(Config)
+
+    mongo.init_app(app)
+
+    app.register_blueprint(user_bp, url_prefix='/api/v1')
+    app.register_blueprint(book_bp, url_prefix='/api/v1')
+
     return app
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(debug=True)
 
