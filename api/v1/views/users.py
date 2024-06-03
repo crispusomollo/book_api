f1b4c7bea8cda0a8248a42fc7f079c2f99e83b63
crispusomollo <crispusomollo@gmail.com>
2024-06-03 09:02:18 +0000

    Updated the user route

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,29 +1,18 @@
 from flask import Blueprint, request, jsonify
-from models.user import User
+from models.models import mongo, User
 
-user_bp = Blueprint('user', __name__)
+users = Blueprint('users', __name__)
 
-@user_bp.route('/users', methods=['POST'])
+@users.route('/get_users', methods=['POST'])
 def create_user():
     data = request.get_json()
-    User.create_user(data)
+    new_user = User(data['username'], data['email'])
+    mongo.db.users.insert_one(new_user.to_dict())
     return jsonify({"message": "User created successfully"}), 201
 
-@user_bp.route('/users/<username>', methods=['GET'])
+@users.route('/get_user/<username>', methods=['GET'])
 def get_user(username):
-    user = User.get_user(username)
+    user = mongo.db.users.find_one({"username": username})
     if user:
         return jsonify(user), 200
     return jsonify({"message": "User not found"}), 404
-
-@user_bp.route('/users/<username>', methods=['PUT'])
-def update_user(username):
-    data = request.get_json()
-    User.update_user(username, data)
-    return jsonify({"message": "User updated successfully"}), 200
-
-@user_bp.route('/users/<username>', methods=['DELETE'])
-def delete_user(username):
-    User.delete_user(username)
-    return jsonify({"message": "User deleted successfully"}), 200
-
